0e0f81e64f2067d6c66e41569c0ac7cac7059d6f
강민서 <kmsmsmy@naver.com>
2023-04-12 02:16:22 +0000

    ##2023년 4월 12일 깃 허브 테스트 완료

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,6 +1,7 @@
 function pop_up() {
 	window.open("../pop_up/pop_up.html", "팝업테스트", "width=400, height=300, top=10, left=10");
 }
+
 function show_clock(){
         let currentDate = new Date(); // 날짜 객체 생성
         let divClock = document.getElementById('divClock');
